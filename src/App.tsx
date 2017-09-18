c76d6716062aedfd17cbac779708c92ba8da5464
Oceanswave <sean.mclellan@sixconcepts.com>
2017-09-17 22:45:14 -0400

    Some initial progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import * as React from 'react';
-import './App.css';
-
-const logo = require('./logo.svg');
+import './App.scss';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+        <nav className="pt-navbar pt-fixed-top pt-dark">
+          <div style={{ margin: '0 auto' }}>
+            <div className="pt-navbar-group pt-align-left">
+              <div className="pt-navbar-heading">Barista Workbench</div>
+            </div>
+            <div className="pt-navbar-group pt-align-right">
+              <button className="pt-button pt-minimal pt-icon-dashboard">Dashboard</button>
+              <button className="pt-button pt-minimal pt-icon-code">Fiddle</button>
+              <span className="pt-navbar-divider" />
+              <button className="pt-button pt-minimal pt-icon-cog" />
+            </div>
+          </div>
+        </nav>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
