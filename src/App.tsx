bc6328421504416439ee211ae16e79b1bd0a5daf
Oceanswave <sean.mclellan@sixconcepts.com>
2017-09-18 10:49:15 -0400

    Add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import * as React from 'react';
 import './App.scss';
 
+import { WorkbenchNavbar } from './components/WorkbenchNavbar';
+
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <nav className="pt-navbar pt-fixed-top pt-dark">
-          <div style={{ margin: '0 auto' }}>
-            <div className="pt-navbar-group pt-align-left">
-              <div className="pt-navbar-heading">Barista Workbench</div>
-            </div>
-            <div className="pt-navbar-group pt-align-right">
-              <button className="pt-button pt-minimal pt-icon-dashboard">Dashboard</button>
-              <button className="pt-button pt-minimal pt-icon-code">Fiddle</button>
-              <span className="pt-navbar-divider" />
-              <button className="pt-button pt-minimal pt-icon-cog" />
-            </div>
-          </div>
-        </nav>
+        <WorkbenchNavbar />
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
---
+++ src/components/WorkbenchNavbar/WorkbenchNavbar.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+const logo = require('./logo.svg');
+
+export class WorkbenchNavbar extends React.Component {
+    render() {
+        return (
+            <nav className="pt-navbar pt-fixed-top pt-dark">
+                <div style={{ margin: '0 auto' }}>
+                    <div className="pt-navbar-group pt-align-left">
+                        <img src={logo} alt="BaristaLabs, LLC" style={{ height: '35px', paddingRight: '10px' }} />
+                        <div className="pt-navbar-heading">Barista Workbench</div>
+                    </div>
+                    <div className="pt-navbar-group pt-align-right">
+                        <button className="pt-button pt-minimal pt-icon-dashboard">Dashboard</button>
+                        <button className="pt-button pt-minimal pt-icon-code">Fiddle</button>
+                        <span className="pt-navbar-divider" />
+                        <button className="pt-button pt-minimal pt-icon-cog" />
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
