157cc673b40881a33d4cba989c3a57d518071915
Oceanswave <sean.mclellan@sixconcepts.com>
2017-09-18 10:58:56 -0400

    Make navbar smaller

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { WorkbenchNavbar } from './components/WorkbenchNavbar';
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <div className="App pt-focus-disabled">
         <WorkbenchNavbar />
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
--- src/components/WorkbenchNavbar/WorkbenchNavbar.tsx
+++ src/components/WorkbenchNavbar/WorkbenchNavbar.tsx
@@ -7,7 +7,7 @@ export class WorkbenchNavbar extends React.Component {
             <nav className="pt-navbar pt-fixed-top pt-dark">
                 <div style={{ margin: '0 auto' }}>
                     <div className="pt-navbar-group pt-align-left">
-                        <img src={logo} alt="BaristaLabs, LLC" style={{ height: '35px', paddingRight: '10px' }} />
+                        <img src={logo} alt="BaristaLabs, LLC" style={{ height: '20px', paddingRight: '5px' }} />
                         <div className="pt-navbar-heading">Barista Workbench</div>
                     </div>
                     <div className="pt-navbar-group pt-align-right">
