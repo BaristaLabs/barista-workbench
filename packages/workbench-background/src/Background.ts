7fe028a8bbdf1d7f757578f1993dffed95299024
Oceanswave <sean.mclellan@sixconcepts.com>
2017-09-19 20:08:01 -0400

    piddling around.

--- packages/workbench-background/src/Background.ts
+++ packages/workbench-background/src/Background.ts
@@ -1,5 +1,15 @@
 chrome.browserAction.onClicked.addListener((tab) => {
-    console.dir('icon clicked.');
+    
+    chrome.tabs.create({ 'url': chrome.extension.getURL(`index.html#/fiddle?source=${tab.id}`), 'selected': true }, (fiddleTab) => {
+        if (!tab.id || !fiddleTab.id) {
+            console.error('The tab upon which the browser action was pressed did not have an id');
+            return;
+        }
 
-    chrome.tabs.create({ 'url': chrome.extension.getURL('index.html'), 'selected': true });
+        const port = chrome.tabs.connect(fiddleTab.id);
+        chrome.tabs.executeScript(tab.id, {file: 'src/contentScript.js'}, (response) => {
+            
+        });
+    });
+
 });
